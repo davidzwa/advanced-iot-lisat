5fc0fd3a8e417aa96e463c736bb0cab7967c48aa
David Zwart <davidzwa@gmail.com>
2020-10-10 22:01:42 +0200

    Worked on audio correlator and mic trigger peripherals

--- ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/adcBufMultiChannel.cpp
+++ ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/adcBufMultiChannel.cpp
@@ -9,10 +9,10 @@
 
 #include <semaphore.h>
 
-/* Driver Header files */
+/* Peripheral Header files */
 #include <ti/drivers/ADCBuf.h>
-
-/* Display Header files */
+#include <ti/drivers/Capture.h>
+#include <ti/drivers/GPIO.h>
 #include <ti/display/Display.h>
 #include <ti/display/DisplayUart.h>
 
@@ -23,13 +23,14 @@
 #include "common.h"
 #include "dsp/iirfilter.h"
 #include "dsp/fft.h"
+#include "corrAudio.h"
 
 #define NUM_ADC_CHANNELS (1)
 #define ADCBUFFERSIZE    (CHUNK_LENGTH)
 
 int16_t sampleBuffer1a[ADCBUFFERSIZE];
 int16_t sampleBuffer1b[ADCBUFFERSIZE];
-int16_t fftOutput[ADCBUFFERSIZE];
+//int16_t fftOutput[ADCBUFFERSIZE];
 #if NUM_ADC_CHANNELS >= 2
 int16_t sampleBuffer2a[ADCBUFFERSIZE];
 int16_t sampleBuffer2b[ADCBUFFERSIZE];
@@ -39,18 +40,38 @@ int16_t sampleBuffer3a[ADCBUFFERSIZE];
 int16_t sampleBuffer3b[ADCBUFFERSIZE];
 #endif
 int32_t buffersCompletedCounter = 0;
+//int16_t correlation[CHUNK_LENGTH];
 int16_t outputBuffer[ADCBUFFERSIZE];
 int16_t outputBuffer_filtered[ADCBUFFERSIZE];
 int16_t rms;
 
 /* Display Driver Handle */
-Display_Handle displayHandle;
+Display_Handle display;
+volatile uint32_t curInterval;
 
 /* ADCBuf semaphore */
 sem_t adcbufSem;
 
 /* DSP LPF Filter */
 IirFilter* filter;
+
+/* Callback used for blinking LED on timer completion */
+//void captureCallback(Capture_Handle handle, uint32_t interval);
+
+/* Callback function that displays the interval */
+void captureCallback(Capture_Handle handle, uint32_t interval)
+{
+    curInterval = interval;
+    GPIO_write(CONFIG_GPIO_0, 0);
+    GPIO_write(CONFIG_GPIO_0, 1);
+//    SemaphoreP_post(captureSem);
+}
+
+void interruptCallback(uint_least8_t index) {
+    /* Clear the GPIO interrupt and toggle an LED */
+    GPIO_toggle(CONFIG_GPIO_LED_1);
+    GPIO_write(CONFIG_GPIO_0, 0);
+}
 
 /*
  * This function is called whenever a buffer is full.
@@ -77,6 +98,8 @@ void adcBufCallback(ADCBuf_Handle handle, ADCBuf_Conversion *conversion,
 void *mainThread(void *arg0)
 {
     Display_Params displayParams;
+//    Capture_Params captureParams;
+//    Capture_Handle capture;
     ADCBuf_Handle adcBuf;
     ADCBuf_Params adcBufParams;
     ADCBuf_Conversion continuousConversion[2];
@@ -85,24 +108,49 @@ void *mainThread(void *arg0)
 
     /* Call driver init functions */
     ADCBuf_init();
+    GPIO_init();
     Display_init();
+//    Capture_init();
+
+    /* Turn on user LED */
+//    GPIO_write(CONFIG_, CONFIG_GPIO_LED_ON);
+    GPIO_write(CONFIG_GPIO_0, 1);
 
     /* Configure & open Display driver */
     Display_Params_init(&displayParams);
     displayParams.lineClearMode = DISPLAY_CLEAR_BOTH;
-    displayHandle = Display_open(Display_Type_UART, &displayParams);
-    if (displayHandle == NULL) {
-        Display_printf(displayHandle, 0, 0, "Error creating displayHandle\n");
+    display = Display_open(Display_Type_UART, &displayParams);
+    if (display == NULL) {
+        Display_printf(display, 0, 0, "Error creating display\n");
         while (1);
     }
     
     status = sem_init(&adcbufSem, 0, 0);
     if (status != 0) {
-        Display_printf(displayHandle, 0, 0, "Error creating adcbufSem\n");
+        Display_printf(display, 0, 0, "Error creating adcbufSem\n");
         while(1);
     }
 
-    Display_printf(displayHandle, 0, 0, "Starting the ADCBuf Multi-Channel example");
+    GPIO_setCallback(INTERRUPT_D_OUT2, interruptCallback);
+    GPIO_enableInt(INTERRUPT_D_OUT2);
+
+    /* Setting up the Capture driver to detect two rising edges and report
+     * the result in microseconds
+     */
+//    Capture_Params_init(&captureParams);
+//    captureParams.mode = Capture_RISING_EDGE;
+//    captureParams.periodUnit = Capture_PERIOD_US;
+//    captureParams.callbackFxn = captureCallback;
+//    capture = Capture_open(CONFIG_CAPTURE_0, &captureParams);
+//    if (capture == NULL)
+//    {
+//        Display_printf(display, 0, 0, "Failed to initialized Capture!\n");
+//        while(1);
+//    }
+    Display_printf(display, 0, 0, "Capture Driver initialized.\n");
+
+    Display_printf(display, 0, 0, "Dual ADCBuf driver initialized.");
+//    Capture_start(capture);
 
     /* Set up an ADCBuf peripheral in ADCBuf_RECURRENCE_MODE_CONTINUOUS */
     ADCBuf_Params_init(&adcBufParams);
@@ -159,20 +207,20 @@ void *mainThread(void *arg0)
         /*
          * Start with a header message and print current buffer values
          */
-//        Display_printf(displayHandle, 0, 0, "\r\n", //Buffer %u finished:
+//        Display_printf(display, 0, 0, "\r\n", //Buffer %u finished:
 //            (unsigned int)buffersCompletedCounter++);
 
 //         filter->FilterBuffer(outputBuffer, outputBuffer_filtered);
         filter->FilterEMABuffer(outputBuffer, outputBuffer_filtered);
 
 //        doFFT(outputBuffer_filtered, fftOutput);
-//        fftOutput[0] = 0;
-        arm_rms_q15(outputBuffer_filtered, ADCBUFFERSIZE, &rms);
+//        arm_correlate_q15(outputBuffer_filtered, CHUNK_LENGTH, audioVector, AUDIO_LENGTH, correlation);
 
-        for (i = 0; i < ADCBUFFERSIZE; i++) {
-            Display_printf(displayHandle, 0, 0, "v.%d", outputBuffer_filtered[i]);
-        }
-        Display_printf(displayHandle, 0, 0, "R.%d", rms);
+//        for (i = 0; i < ADCBUFFERSIZE; i++) {
+//            Display_printf(display, 0, 0, "v.%d", outputBuffer_filtered[i]);
+//        }
+        arm_rms_q15(outputBuffer_filtered, ADCBUFFERSIZE, &rms);
+        Display_printf(display, 0, 0, "R.%d", rms);
 //        if (rms > 0) {
 //
 //        }
@@ -181,8 +229,8 @@ void *mainThread(void *arg0)
 //        arm_max_q15(outputBuffer, ADCBUFFERSIZE, &maxValue, &maxIndex);
 
 //        outputBuffer_filtered[]
-//        Display_printf(displayHandle, 0, 0, "%d", maxValue);
-        Display_printf(displayHandle, 0, 0, "--Done");
+//        Display_printf(display, 0, 0, "%d", maxValue);
+        Display_printf(display, 0, 0, "--Done");
         numBufsSent++;
     }
 
--- ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/common.h
+++ ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/common.h
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <arm_const_structs.h>
 
-#define CHUNK_LENGTH     256
+#define CHUNK_LENGTH     512
 #define SAMPLE_FREQUENCY (10000)
 
 #endif // COMMON__H
