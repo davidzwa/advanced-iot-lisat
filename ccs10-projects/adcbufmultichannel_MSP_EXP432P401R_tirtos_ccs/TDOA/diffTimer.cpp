6aef16cb159a6c541510a1082affb5b0c09a4bfe
David Zwart <davidzwa@gmail.com>
2020-10-11 17:40:06 +0200

    Fixed timer crash

--- ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/TDOA/diffTimer.cpp
+++ ccs10-projects/adcbufmultichannel_MSP_EXP432P401R_tirtos_ccs/TDOA/diffTimer.cpp
@@ -1,6 +1,14 @@
 #include <TDOA/diffTimer.h>
 #include "common.h"
 
+void initTimerParams(){
+    Timer_Params_init(&params);
+//    params.period = 1000000;
+    params.periodUnits = Timer_PERIOD_US;
+    params.timerMode = Timer_FREE_RUNNING;
+//    params.timerCallback = timerCallback;
+}
+
 void initTimer() {
     /* Turn off user LED */
     GPIO_write(LED_TRIGGER_1, 0);
@@ -8,20 +16,12 @@ void initTimer() {
     /* Setting up the timer in continuous callback mode that calls the callback
      * function every 1,000,000 microseconds, or 1 second.
      */
+    initTimerParams();
     timer0 = Timer_open(CONFIG_TIMER_0_US_MEASURE, &params);
-
     if (timer0 == NULL) {
         /* Failed to initialized timer */
         while (1) {}
     }
-}
-
-void initTimerParams(){
-    Timer_Params_init(&params);
-//    params.period = 1000000;
-    params.periodUnits = Timer_PERIOD_US;
-    params.timerMode = Timer_FREE_RUNNING;
-//    params.timerCallback = timerCallback;
 }
 
 void startTimer() {
