a6862ea64350d2b242d05773bdc9a60c68409cb2
David Zwart <davidzwa@gmail.com>
2020-10-27 19:05:12 +0100

    Move microphone timer to timer32, use free timer for tacho

--- src/MSP432LISAT/MSP432P401R_LISAT_ccs/Robot/tachometer.cpp
+++ src/MSP432LISAT/MSP432P401R_LISAT_ccs/Robot/tachometer.cpp
@@ -5,47 +5,53 @@
  */
 
 #include <ti/sysbios/knl/Clock.h>
+#include <ti/drivers/Timer.h>
 #include "common.h"
+#include "TDOA/freeRunningTimer.h"
 
 uint32_t lastTicksLeft = 0;
 uint32_t lastTicksRight = 0;
 uint32_t currentTicksDiffLeft = 0;
 uint32_t currentTicksDiffRight = 0;
 
-#define DROP_SAMPLE_ABOVE 100
+#define DROP_SAMPLE_ABOVE 30000
 
 void tachoLeftInterrupt(uint_least8_t index);
 void tachoRightInterrupt(uint_least8_t index);
 
 void initTachometerInterrupts() {
+    initTimerTacho();
+    startTimerTacho();
+
     // Setup the hooks for the tachometers ISRs
     GPIO_setCallback(TACHO_LEFT, tachoLeftInterrupt);
     GPIO_setCallback(TACHO_RIGHT, tachoRightInterrupt);
 
-    lastTicksLeft = Clock_getTicks();
-    lastTicksRight = Clock_getTicks();
+    lastTicksLeft = getTimerUsTacho(); // Clock_getTicks();
+    lastTicksRight = getTimerUsTacho(); // Clock_getTicks();
 
     GPIO_enableInt(TACHO_LEFT);
     GPIO_enableInt(TACHO_RIGHT);
 }
 
 void tachoLeftInterrupt(uint_least8_t index) {
-    uint32_t clockValue = Clock_getTicks();
-    if (clockValue - lastTicksLeft < DROP_SAMPLE_ABOVE) {
-        currentTicksDiffLeft = clockValue - lastTicksLeft;
+    uint32_t clockValue = getTimerUsTacho();
+    if (clockValue < lastTicksLeft) {
+        currentTicksDiffLeft = clockValue + (0xFFFF - lastTicksLeft);
     }
     else {
-        currentTicksDiffLeft = 0;
+        currentTicksDiffLeft = clockValue - lastTicksLeft;
     }
     lastTicksLeft = clockValue;
 }
 
 void tachoRightInterrupt(uint_least8_t index) {
-    uint32_t clockValue = Clock_getTicks();
-    if (clockValue - lastTicksRight < DROP_SAMPLE_ABOVE) {
+    uint32_t clockValue = getTimerUsTacho();
+    if (clockValue < lastTicksRight) {
+        currentTicksDiffRight = clockValue + (0xFFFF - lastTicksRight);
+    }
+    else {
         currentTicksDiffRight = clockValue - lastTicksRight;
-    } else {
-        currentTicksDiffRight = 0;
     }
     lastTicksRight = clockValue;
 }
