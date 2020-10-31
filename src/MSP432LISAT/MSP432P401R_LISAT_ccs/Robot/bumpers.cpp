2e7669fa3a7e379d732a3db27ccbe9d15e00ea76
Tomas van Rietbergen <tomas.rietbergen@gmail.com>
2020-10-31 19:56:35 +0100

    added debouncing for bumper triggers and share single callback function for all bumpers

--- src/MSP432LISAT/MSP432P401R_LISAT_ccs/Robot/bumpers.cpp
+++ src/MSP432LISAT/MSP432P401R_LISAT_ccs/Robot/bumpers.cpp
@@ -7,26 +7,36 @@
 
 #include <Robot/robot.h>
 #include <Robot/bumpers.h>
+#include <ti/sysbios/knl/Clock.h>
 
-void bumper0RightInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
-void bumper1RightInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
-void bumper2RightInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
-void bumper3LeftInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
-void bumper4LeftInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
-void bumper5LeftInterrupt(uint_least8_t index) {
-    GPIO_toggle(LED_BLUE_0_GPIO);
-}
+long bumper_last_called = 0;
 
+void bumperInterrupt(uint_least8_t index) {
+    if (Clock_getTicks() - bumper_last_called < 1000) { // every second
+        return;
+    }
+    switch(index) {
+        case BUMPER_0_RIGHT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+        case BUMPER_1_RIGHT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+        case BUMPER_2_RIGHT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+        case BUMPER_3_LEFT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+        case BUMPER_4_LEFT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+        case BUMPER_5_LEFT:
+            GPIO_toggle(LED_BLUE_0_GPIO);
+            break;
+    }
+    bumper_last_called = Clock_getTicks();
+}
 
 void enableBumperInterrupts() {
     GPIO_enableInt(BUMPER_0_RIGHT);
@@ -47,12 +57,12 @@ void disableBumperInterrupts() {
 }
 
 void initBumpers() {
-    GPIO_setCallback(BUMPER_0_RIGHT, bumper0RightInterrupt);
-    GPIO_setCallback(BUMPER_1_RIGHT, bumper1RightInterrupt);
-    GPIO_setCallback(BUMPER_2_RIGHT, bumper2RightInterrupt);
-    GPIO_setCallback(BUMPER_3_LEFT, bumper3LeftInterrupt);
-    GPIO_setCallback(BUMPER_4_LEFT, bumper4LeftInterrupt);
-    GPIO_setCallback(BUMPER_5_LEFT, bumper5LeftInterrupt);
+    GPIO_setCallback(BUMPER_0_RIGHT, bumperInterrupt);
+    GPIO_setCallback(BUMPER_1_RIGHT, bumperInterrupt);
+    GPIO_setCallback(BUMPER_2_RIGHT, bumperInterrupt);
+    GPIO_setCallback(BUMPER_3_LEFT, bumperInterrupt);
+    GPIO_setCallback(BUMPER_4_LEFT, bumperInterrupt);
+    GPIO_setCallback(BUMPER_5_LEFT, bumperInterrupt);
 
 }
 
