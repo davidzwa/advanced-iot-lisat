9ab9108e3e002bf9f35505bf0ddf648b90af3bf9
tomasvr <tomas.rietbergen@gmail.com>
2020-11-13 00:41:32 +0100

    Implement timer32 for high resolution timer interrupts for IR readings

--- src/MSP432LISAT/MSP432P401R_LISAT_ccs/System/IrSensorsTimer.h
+++ src/MSP432LISAT/MSP432P401R_LISAT_ccs/System/IrSensorsTimer.h
@@ -5,17 +5,18 @@
  *      Author: Tomas
  */
 
+#include <ti/drivers/Timer.h>
+
 #ifndef SYSTEM_IRSENSORSTIMER_H_
 #define SYSTEM_IRSENSORSTIMER_H_
 
-#include <ti/drivers/Timer.h>
+Timer_Handle irTimer;
+Timer_Params irTimerParams;
 
-Timer_Handle IrTimer;
-Timer_Params params32;
-
-void initTimerIr();
-
+void initIrTimer(Timer_CallBackFxn callback);
+void irTimerStart();
+void irTimerStop();
+void irTimerSetPeriodUs(uint32_t periodUs);
 uint32_t getTimerCountsIr();
-
 
 #endif /* SYSTEM_IRSENSORSTIMER_H_ */
