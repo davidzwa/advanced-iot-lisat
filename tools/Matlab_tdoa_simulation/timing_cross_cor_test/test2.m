98809377cc8e1e9366c186a38e96d26e7be7eabf
David Zwart <davidzwa@gmail.com>
2020-11-18 16:00:36 +0100

    Squash and cherrypick MATLAB changes

--- tools/Matlab_tdoa_simulation/timing_cross_cor_test/test2.m
+++ tools/Matlab_tdoa_simulation/timing_cross_cor_test/test2.m
@@ -1,9 +1,9 @@
 clearvars
 close all
-
+% opengl('software')
 Fs = 44000;
 %% Pulse generation
-pulse_length = 0.002;
+pulse_length = 0.017;
 chirp_f0 = 1500;
 chirp_f1 = 6000;
 
@@ -11,8 +11,8 @@ T_pulse = 0: 1/Fs: pulse_length-1/Fs;
 Y_pulse = chirp(T_pulse,chirp_f0,pulse_length,15000);
 
 %% Signal generation
-signal_length = 0.017;
-inter_pulse_time = 0.001;
+signal_length = 0.06;
+inter_pulse_time = 0.003;
 
 pulse_times =1 + floor((0: (pulse_length + inter_pulse_time): (signal_length - pulse_length)) * Fs);
 
@@ -28,12 +28,13 @@ end
 plot(T_signal, Y_signal);
 
 %% Propogate signal (time shift, attinuation and noise)
-buffer_length_samples = 1024;
+buffer_length_samples = 3000;
 
 buffers = zeros(3,buffer_length_samples);
 
-sample_delays = randi([50, 150]) + [0, randi(24,1,2)];
-snr = [1,5,10];
+sample_delays = randi([50, 100]) + [0, randi(75,1,2)];
+snr = 0.0001*ones(1,3);%[1,5,10];
+attenuation = [1, 0.8, 0.6];
 
 % True TDOAs form sample delays, with value 1 as reference
 TDOAs_true = sample_delays(2:end) - sample_delays(1);
@@ -41,6 +42,7 @@ TDOAs_true = sample_delays(2:end) - sample_delays(1);
 for i = 1:3
     buffers(i,sample_delays(i): sample_delays(i) + length(Y_signal) -1) = Y_signal;
     buffers(i,:) = awgn(buffers(i,:),snr(i));
+    buffers(i,:) = attenuation(i).*buffers(i,:);
 end
 
 figure
@@ -56,12 +58,22 @@ zero_range = floor(0.4*inter_pulse_samples);
 
 signal_centers = zeros(1,3);
 
+f(1) = figure;
+f(2) = figure;
+f(3) = figure;
+f(4) = figure('Renderer', 'painters', 'Position', [10 10 800 300]);
+
 for i = 1:3
     % Per buffer (= per microphone)
     % crosscorrelate with chirp pulse
     [cor_dat,cor_lag] = xcorr_simp(Y_pulse, buffers(i,:));
     
-    figure
+    figure(f(4))
+    subplot(3,1,i)
+    stem(cor_lag, cor_dat,'Marker','none', "DisplayName", "Correlation results")
+    
+    
+    figure(f(i))
     subplot(2,1,1)
     stem(cor_lag, cor_dat)
     
@@ -86,6 +98,19 @@ for i = 1:3
     stem(cor_lag, cor_dat);
     hold on
     stem(pulse_delays, pulse_values)    
+    
+    figure(f(4))
+    subplot(3,1,i)
+    hold on
+    stem(pulse_delays, pulse_values,':o', "DisplayName", "Found chirps")
+    xline(mean(pulse_delays),"DisplayName", "Determined arrival time","LineWidth", 1)
+%     ylabel("Corrolation value")
+    if i == 3
+        xlabel("Sample delay")
+    end
+    legend('show')
+    ylim([-100, 500])
+    xlim([-700, 3500])
 end
 
 TDOAs_result = signal_centers(2:end) - signal_centers(1);
