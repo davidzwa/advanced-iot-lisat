600cea3f19c35fa5138e44c183591cda0f3348d5
David Zwart <davidzwa@gmail.com>
2020-11-22 15:51:47 +0100

    Add control stub

---
+++ src/MSP432LISAT/MSP432P401R_LISAT_ccs/Robot/robotDriveControl.cpp
@@ -0,0 +1,15 @@
+/*
+ * robotDriveControl.cpp
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: d.zwart
+ */
+
+#include <Robot/robotDriveControl.h>
+
+RobotDriveControl::RobotDriveControl()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
