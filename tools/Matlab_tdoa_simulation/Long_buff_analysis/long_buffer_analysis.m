33be411018073c2d1c4de3306a2b7876e6c7d749
David Zwart <davidzwa@gmail.com>
2020-11-23 02:58:39 +0100

    MATLAB chirp detection

--- tools/Matlab_tdoa_simulation/Long_buff_analysis/long_buffer_analysis.m
+++ tools/Matlab_tdoa_simulation/Long_buff_analysis/long_buffer_analysis.m
@@ -1,15 +1,17 @@
 close all
 clearvars
 
-[file, path] = uigetfile("*.csv");
+[file, path] = uigetfile("C:\Users\caspe\Documents\TU_Delft\Master\Y2\Q5\advanced_IoT\advanced-iot-lisat\data\output_recordings_ccs10_chirp\*.csv");
 file = convertCharsToStrings(file);
 path = convertCharsToStrings(path);
 
 addpath("..");
 addpath("../timing_cross_cor_test");
 
-Fs = 39000;
+Fs = 27000;%39000;
 [u, ~] = gen_chirp(1000, 2000, 0.017, 0.017, Fs);
+
+u = floor(u*(2^10 - 1));
 figure(1)
 subplot(4,1,4)
 plot(u)
@@ -21,16 +23,16 @@ zero_range = ceil(0.005 * Fs);
 full_path = path + file;
 
 data = readmatrix(full_path);
-buffer_length = length(data) / 3;
+buffer_length = 1600;%length(data) / 3;
 
 D = zeros(3,buffer_length);
-for i = 1:3
+for i = 1:1
     D(i,:) = data((i-1)*buffer_length + 1:   i*buffer_length ,2);
     figure(1)
     subplot(4,1,i)
     plot(D(i,:))
     
-    [cor_dat,cor_lag] = xcorr_simp(u,D(i,:));
+    [cor_dat,cor_lag] = xcorr(u,D(i,:));
     figure(2)
     subplot(3,1,i)
     stem(cor_lag,cor_dat,'marker','none')
@@ -55,6 +57,7 @@ for i = 1:3
     figure(2)
     hold on
     stem(pulse_delays, pulse_values) 
+    xline(signal_centers(i))
     
 end
 
--- tools/Matlab_tdoa_simulation/timing_cross_cor_test/gen_chirp_script.m
+++ tools/Matlab_tdoa_simulation/timing_cross_cor_test/gen_chirp_script.m
@@ -1,12 +1,12 @@
 clearvars
 close all
 
-Fs = 39000;
+Fs = 27000;
 pulse_length = 0.017;
 chirp_f0 = 1000;
 chirp_f1 = 2000;
 
-max_range = 2^15 - 1;
+max_range = 2^10 - 1;
 
 T_pulse = 0: 1/Fs: pulse_length-1/Fs;
 Y_pulse = (max_range * chirp(T_pulse,chirp_f0,pulse_length,15000));
@@ -16,8 +16,8 @@ plot(T_pulse, Y_pulse)
 hold on
 plot(T_pulse, Y_pulse_int)
 
-fprintf("const int16_t chirp[] = {")
+fprintf("q15_t chirp[] = {")
 for i = 1:length(Y_pulse_int)-1
    fprintf("%d, ", Y_pulse_int(i));
 end
-fprintf("%d};\n", Y_pulse_int(end));
+fprintf("%d};\nuint16_t chirp_length = %d;\n", Y_pulse_int(end),length(Y_pulse_int));
